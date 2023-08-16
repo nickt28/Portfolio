663c8a7a0e9642448dd20b28d2be748c05b67dfa
Nick T <93458816+nickt28@users.noreply.github.com>
2023-08-16 16:17:35 +1000

    Added stars movement

--- stars.js
+++ stars.js
@@ -2,7 +2,7 @@
 const ctx = canvas1.getContext("2d")
 let pageWidth = window.innerWidth
 let pageHeight = window.innerHeight
-let starCount = 0
+let stars = []
 
 function resizeCanvas() {
   pageWidth = window.innerWidth
@@ -10,29 +10,53 @@ function resizeCanvas() {
   canvas1.width = pageWidth
   canvas1.height = pageHeight
 }
-resizeCanvas()
 
 window.addEventListener("resize", function () {
   resizeCanvas()
-  initiateStars()
+  resetStarsYCords()
 })
 
+function resetStarsYCords() {
+  for (let i=0, l=stars.length; i<l; i++) {
+    stars[i].x = pageWidth * Math.random()
+  }
+}
+
 function initiateStars() {
-  for (let i=0; i < 250; i++) {
+  for (let i=0; i < 500; i++) {
     addStar()
   }
 }
-initiateStars()
 
+// Add a star to the stars array
 function addStar() {
-  starCount++
-  ctx.fillStyle = "white";
-  ctx.shadowBlur = 10;
-  ctx.shadowColor = "white";
-  let x = pageWidth * Math.random();
-  let y = pageHeight * Math.random();
-  let r = 3 * Math.random();
-  ctx.beginPath();
-  ctx.arc(x, y, r, 0, Math.PI * 2);
-  ctx.fill();
+  let x = pageWidth * Math.random()
+  let y = pageHeight * Math.random()
+  let r = 3 * Math.random()
+  let speed = 0.8 * Math.random() // random speed between 1 and 4 pixels per frame
+  stars.push({ x, y, r, speed })
 }
+
+// Draw the stars in stars array on the canvas
+function updateStars() {
+  ctx.clearRect(0, 0, pageWidth, pageHeight)
+  for (let i = 0; i < stars.length; i++) {
+    let star = stars[i]
+    star.y -= star.speed
+    if (star.y < -star.r) {
+      // if star goes off the top of the screen, reset its position to the bottom
+      star.y = pageHeight + star.r
+    }
+    ctx.fillStyle = "white"
+    ctx.shadowBlur = 10
+    ctx.shadowColor = "white"
+    ctx.beginPath()
+    ctx.arc(star.x, star.y, star.r, 0, Math.PI * 2)
+    ctx.fill()
+  }
+  requestAnimationFrame(updateStars)
+}
+
+resizeCanvas()
+initiateStars()
+updateStars()

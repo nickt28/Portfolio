ee2236767053070873c3289d00a028b75bdfb383
Nick T <93458816+nickt28@users.noreply.github.com>
2023-09-05 14:16:33 +1000

    config

--- weather/config.js
+++ weather/config.js
@@ -1,8 +1,8 @@
-const OPEN_WEATHER_MAP_API_KEY = 'IMPORT-YOUR-KEY';
+const OPEN_WEATHER_MAP_API_KEY = '58598ecc7b95ec78992388426bbfc196';
 
 // Create the script tag, set the appropriate attributes
 var script = document.createElement('script');
-script.src = 'https://maps.googleapis.com/maps/api/js?key=IMPORT-YOUR-KEY&libraries=places&callback=initMap';
+script.src = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyCllC58k5ap3vMqyKsmtKnnTPrBzdczJ2A&libraries=places&callback=initMap';
 script.async = true;
 
 // Attach your callback function to the `window` object

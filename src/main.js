19e27c2ff0c67d18e6242ddfcef79a06db05e0e9
Nick Tomkins <93458816+nickt28@users.noreply.github.com>
2024-02-18 13:23:48 +1100

    Security and feature
    Removed inline JS
    Index Content-Security-Policy
    ParcelJS Update
    Background stars improvements

--- src/main.js
+++ src/main.js
@@ -1,72 +1,82 @@
-const votal = document.getElementById('votal')
+// PDF Height
+document.getElementById('pdf').addEventListener('load', function() {
+  this.style.height = '800px';
+});
+
+// Smooth scroll to project cards
 document.getElementById('project-card-votal').addEventListener('click', function () {
-    votal.scrollIntoView({ behavior: 'smooth' });
+  document.getElementById('votal').scrollIntoView({ behavior: 'smooth' });
 })
 
-// Creaet stars animation
-const ctx = canvas1.getContext("2d")
-let pageWidth = window.innerWidth
-let pageHeight = window.innerHeight
+// Create a new canvas element
+let newCanvas = document.createElement('canvas');
+newCanvas.id = 'canvas1';
+document.getElementById('background').appendChild(newCanvas);
+
+// Create stars animation
+let canvas1 = document.getElementById('canvas1');
+let ctx = canvas1.getContext('2d');
+
 let stars = []
 
 function resizeCanvas() {
-  pageWidth = window.innerWidth
-  pageHeight = window.innerHeight
-  canvas1.width = pageWidth
-  canvas1.height = pageHeight
+  canvas1.width = window.innerWidth;
+  canvas1.height = window.innerHeight;
 }
 
 window.addEventListener("resize", function () {
+  console.log('resizeCanvas')
+  oldX = canvas1.width;
   resizeCanvas()
-  resetStarsYCords()
+  resetStarsXCords(oldX)
 })
 
-function resetStarsYCords() {
-  for (let i=0, l=stars.length; i<l; i++) {
-    stars[i].x = pageWidth * Math.random()
+function resetStarsXCords(oldX) {
+  let widthChanged = oldX !== window.innerWidth;
+  if (widthChanged) {
+    for (let i=0, l=stars.length; i<l; i++) {
+      stars[i].x *= canvas1.width / oldX;
+    }
   }
 }
 
 function initiateStars() {
-  for (let i=0; i < 500; i++) {
-    addStar()
+  let numStars = canvas1.width < 800 ? 250 : 500;
+  for (let i=0; i < numStars; i++) {
+    addStar();
   }
 }
 
 // Add a star to the stars array
 function addStar() {
-  let x = pageWidth * Math.random()
-  let y = pageHeight * Math.random()
+  let x = canvas1.width * Math.random()
+  let y = canvas1.height * Math.random()
   let r = 3 * Math.random()
-  let speed = pageHeight/10000*(1+Math.random())
+  let speed = canvas1.height/10000*(1+Math.random())
   stars.push({ x, y, r, speed })
 }
 
 // Draw the stars in stars array on the canvas
 function updateStars() {
-  ctx.clearRect(0, 0, pageWidth, pageHeight)
-  
-  stars.forEach(updateStar)
+  ctx.clearRect(0, 0, canvas1.width, canvas1.height)
   
   ctx.fillStyle = "#ffffff21"
   ctx.beginPath()
   const endAngle = Math.PI * 2
+
   stars.forEach(star => {
+    // move the star up the canvas by the star's speed
+    star.y -= star.speed
+    // if the star goes off the top of the canvas + the star's radius, reset it to the bottom
+    if (star.y < -star.r) {
+      star.y = canvas1.height
+    }
     ctx.moveTo(star.x + star.r, star.y)
     ctx.arc(star.x, star.y, star.r, 0, endAngle)
   })
+  
   ctx.fill()
-
   requestAnimationFrame(updateStars)
-}
-
-function updateStar(star) {
-  // move the star up the canvas by the star's speed
-  star.y -= star.speed
-  // if the star goes off the top of the canvas + the star's radius, reset it to the bottom
-  if (star.y < -star.r) {
-    star.y = pageHeight
-  }
 }
 
 resizeCanvas()

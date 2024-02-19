c7194801916897acb93d00b345386238d7ea7e93
Nick Tomkins <93458816+nickt28@users.noreply.github.com>
2024-02-19 14:21:15 +1100

    Add dark mode
    - Add dark mode functionality to the site
    - Refactor star's function to allow any colour and toggle with dark mode
    - Remove SVG images and embed them directly to use fill for dark mode
    - Implement dark mode toggle and refactor to use a class observer model
    - Add tailwind dark mode and star colour change to DarkMode observer
    - Add light mode scheme for all site elements

--- src/main.js
+++ src/main.js
@@ -1,12 +1,76 @@
 // PDF Height
 document.getElementById('pdf').addEventListener('load', function() {
   this.style.height = '800px';
+  this.style.boxShadow = 'rgb(0 0 0 / 15%) 0px 10px 15px 0px';
 });
 
 // Smooth scroll to project cards
 document.getElementById('project-card-votal').addEventListener('click', function () {
   document.getElementById('votal').scrollIntoView({ behavior: 'smooth' });
 })
+
+class DarkMode {
+  constructor() {
+    this.darkMode = localStorage.getItem('darkMode');
+    this.observer = [];
+
+    // Check if the user has set a preference for dark mode
+    if (this.darkMode === null && window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+      this.darkMode = 'true';
+      localStorage.setItem('darkMode', this.darkMode);
+    }
+  }
+
+  toggleDarkModeClass(docClassList) {
+    if (this.isDark() === 'true') {
+      docClassList.add('dark');
+    } else {
+      docClassList.remove('dark');
+    }
+  }
+
+  setupToggle(checkbox, docClassList) {
+    checkbox.addEventListener('change', () => {
+      this.toggle();
+    });
+  }
+
+  addObserver(action) {
+    this.observer.push(action);
+  }
+
+  notifyObservers() {
+    this.observer.forEach(actions => actions(this.darkMode));
+  }
+
+  toggle() {
+    this.darkMode = this.darkMode === 'true' ? 'false' : 'true';
+    localStorage.setItem('darkMode', this.darkMode);
+    this.notifyObservers();
+  }
+
+  isDark() {
+    return this.darkMode;
+  }
+}
+
+// Dark mode setup
+const docClassList = document.documentElement.classList
+const checkbox = document.getElementById('switch');
+
+const darkMode = new DarkMode();
+darkMode.setupToggle(checkbox, docClassList);
+
+// Apply dark mode if it's enabled
+if (darkMode.isDark() === 'true') {
+  darkMode.toggleDarkModeClass(docClassList)
+  checkbox.checked = true;
+}
+
+// Add background-color observer
+darkMode.addObserver(() => {
+  darkMode.toggleDarkModeClass(docClassList);
+});
 
 // Create a new canvas element
 let newCanvas = document.createElement('canvas');
@@ -41,7 +105,7 @@ function resetStarsXCords(oldX) {
 }
 
 function initiateStars() {
-  let numStars = canvas1.width < 800 ? 250 : 500;
+  let numStars = canvas1.width < 800 ? 100 : 300;
   for (let i=0; i < numStars; i++) {
     addStar();
   }
@@ -56,11 +120,19 @@ function addStar() {
   stars.push({ x, y, r, speed })
 }
 
-// Draw the stars in stars array on the canvas
+// Set the color of the stars and add to darkMode observer
+let starColor = "#dcdcdc";
+if (darkMode.isDark() === 'true') {
+  starColor = "#5f9ea0";
+}
+
+darkMode.addObserver(() => {
+  starColor = darkMode.isDark() === 'true' ? "#5f9ea0" : "#dcdcdc";
+});
+
 function updateStars() {
   ctx.clearRect(0, 0, canvas1.width, canvas1.height)
-  
-  ctx.fillStyle = "#ffffff21"
+  ctx.fillStyle = starColor
   ctx.beginPath()
   const endAngle = Math.PI * 2
 
